a359d3e4b1e1e621532479d5b4cc3feade54ac62
theakanksha02 <theakankshaa@gmail.com>
2017-05-05 21:08:38 +0530

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/toolbar/MainActivity.java
@@ -0,0 +1,16 @@
+package com.bignerdranch.android.toolbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+private Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar= (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+    }
+}
